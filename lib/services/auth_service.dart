fd391677bb78dcc2d94c203d748a7763cbca1dc7
QuicaMaycol <jeanchamorro2006gmail.com>
2026-01-19 10:08:14 -0500

    segundo avancesistema de votación

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../repositories/auth_repository.dart';
 import '../models/perfil_empresa.dart';
